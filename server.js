9f58a1b06cd8a307c6af273ffb1f3195387938d1
Mark T <aydenthos@gmail.com>
2023-07-24 22:08:47 -0700

    cleaned up some spacing and indenting

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Middleware for serving all the files in the public folder
 app.use(express.static('public'));
 
 // Middleware for serving all the files in the public folder
 app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
+
  // Listener
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
