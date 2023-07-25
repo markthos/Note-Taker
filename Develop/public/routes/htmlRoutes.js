8a8a8a3d8c9d9da5c0dd3f90fbf3446561285c17
Mark T <aydenthos@gmail.com>
2023-07-24 17:09:27 -0700

    fixed my routing errors and structure issues

--- Develop/public/routes/htmlRoutes.js
+++ Develop/public/routes/htmlRoutes.js
@@ -1,15 +1,15 @@
-const router = require('express').Router();
 const path = require('path');
+const express = require('express');
+const router = express.Router();
 
 // GET Route for homepage
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // GET Route for notes page
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../notes.html'));
 });
 
 module.exports = router;
-   
